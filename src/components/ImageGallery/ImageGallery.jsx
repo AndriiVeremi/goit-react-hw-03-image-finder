404137b9fe946ac1129587ac495ee99a42539f72
Andrii Veremii <dashuk10@gmail.com>
2023-05-14 13:44:15 +0300

    add

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -39,7 +39,7 @@ class ImageGallery extends Component {
         const nextName = this.props.searchFoto;
 
         if (prevName !== nextName || prevState.page !== page) {
-            this.setState({ status: 'pending', value: null });
+            this.setState({ status: 'pending'});
 
             if (this.state.error) {
                 this.setState({ error: null });
